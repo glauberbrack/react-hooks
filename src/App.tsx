0d755ea3dd458e49f366ed016ffa777be4aec49a
glauberbrack <glauber@brack.com.br>
2022-08-23 20:29:26 -0300

    feat: useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hi, there!</div>;
+  const [counter, setCounter] = useState<number>(0);
+
+  function incrementCounter() {
+    const newValue = counter + 1;
+    setCounter(newValue);
+    console.log(counter);
+  }
+  return (
+    <div className="App">
+      Hi, there!
+      <p>Counter: {counter}</p>
+      <button onClick={incrementCounter}>Increase counter!</button>
+    </div>
+  );
 }
 
 export default App;
