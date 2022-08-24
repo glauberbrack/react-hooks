1aca291f6e37735ec3dcd0f6641818bd58f23d63
glauberbrack <glauber@brack.com.br>
2022-08-23 22:57:35 -0300

    feat: useMemo hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,23 @@
-import { useEffect, useState } from "react";
+import { useState, useMemo } from "react";
 import "./App.css";
 
+function Component(props: { title: string | number }) {
+  return <h1>{props.title}</h1>;
+}
+
 function App() {
-  const [counter, setCounter] = useState<number>(0);
+  const [title, setTitle] = useState<string>("");
 
-  function incrementCounter() {
-    setCounter((prevCounter) => prevCounter + 1);
-  }
-
-  useEffect(() => {
-    // Renders when component mount
-    console.log("Component Did Mount");
-  }, [counter]);
-
-  useEffect(() => {
-    // Renders when component will unmount
-    return () => {
-      console.log("Component will unmont");
-    };
+  const result = useMemo(() => {
+    console.log("call useMemo");
+    return 123456789 * 123456789;
   }, []);
 
   return (
     <div className="App" style={{ paddingTop: 20 }}>
-      <p style={{ fontWeight: 600 }}>Get input value using useRef</p>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-        }}
-      >
-        <p>Counter: {counter}</p>
-        <button
-          style={{ height: 25, marginLeft: 10 }}
-          onClick={incrementCounter}
-        >
-          Increment Value
-        </button>
-      </div>
+      <input type="text" onChange={(event) => setTitle(event.target.value)} />
+      <Component title={title} />
+      <p>Result: {result}</p>
     </div>
   );
 }
