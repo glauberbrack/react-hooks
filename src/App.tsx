3704fbf59fbf87d205ac2f33ea5a43ea969dad62
glauberbrack <glauber@brack.com.br>
2022-08-23 23:25:09 -0300

    feat: useReducer hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import { useState, useCallback } from "react";
+import { useReducer } from "react";
 import "./App.css";
 
-function App() {
-  const [counter, setCounter] = useState<number>(0);
+type ActionsTypes = "SUM" | "SUB" | "MULT";
 
-  const incrementCounter = useCallback(() => {
-    console.log("call useCallback");
-    setCounter((prevCounter) => prevCounter + 1);
-  }, []);
+function counterReducer(state: number, action: { type: ActionsTypes }) {
+  const actions = {
+    SUM: state + 1,
+    SUB: state - 1,
+    MULT: state * 10,
+  };
+
+  return actions[action.type];
+}
+
+function App() {
+  const [counter, dispatchCounter] = useReducer(counterReducer, 0);
 
   return (
     <div className="App" style={{ paddingTop: 20 }}>
-      <button onClick={incrementCounter}>Increment Counter</button>
+      <button onClick={() => dispatchCounter({ type: "SUM" })}>
+        Increment 1
+      </button>
+      <button onClick={() => dispatchCounter({ type: "SUB" })}>
+        Decrement 1
+      </button>
+      <button onClick={() => dispatchCounter({ type: "MULT" })}>
+        Multiple by 10
+      </button>
       <p>Result: {counter}</p>
     </div>
   );
