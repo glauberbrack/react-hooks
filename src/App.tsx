906851b4b4d27c5b3032f7a7f86d5ff74b7834e1
glauberbrack <glauber@brack.com.br>
2022-08-23 22:26:08 -0300

    feat: useEffect hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import { useRef } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const ref = useRef<HTMLInputElement>(null);
-  const counter = useRef<number>(0);
-
-  function logValue() {
-    // This will log our component reference
-    // console.log(ref);
-
-    const value = ref.current?.value;
-    console.log("input value", value);
-  }
+  const [counter, setCounter] = useState<number>(0);
 
   function incrementCounter() {
-    counter.current = counter.current + 1;
-    console.log("counter", counter.current);
+    setCounter((prevCounter) => prevCounter + 1);
   }
+
+  useEffect(() => {
+    // Renders when component mount
+    console.log("Component Did Mount");
+  }, [counter]);
+
+  useEffect(() => {
+    // Renders when component will unmount
+    return () => {
+      console.log("Component will unmont");
+    };
+  }, []);
 
   return (
     <div className="App" style={{ paddingTop: 20 }}>
@@ -28,41 +30,13 @@ function App() {
           alignItems: "center",
         }}
       >
-        <p>Email:</p>
-        <input
-          id="email"
-          style={{ height: 20, marginLeft: 10 }}
-          type="text"
-          ref={ref}
-        />
-        <button style={{ height: 25, marginLeft: 10 }} onClick={logValue}>
-          Log value
-        </button>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <p style={{ fontWeight: 600 }}>
-          Increment value only in application (it will not render on frontend)
-        </p>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          }}
+        <p>Counter: {counter}</p>
+        <button
+          style={{ height: 25, marginLeft: 10 }}
+          onClick={incrementCounter}
         >
-          <p>Counter:</p>
-          <button
-            style={{ height: 25, marginLeft: 10 }}
-            onClick={incrementCounter}
-          >
-            Increment value
-          </button>
-        </div>
+          Increment Value
+        </button>
       </div>
     </div>
   );
