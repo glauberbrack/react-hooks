d17e2cf810e5c471fcd033184493eacf989ccd35
glauberbrack <glauber@brack.com.br>
2022-08-24 23:29:58 -0300

    feat: useTransition hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import { useState } from "react";
+import { useState, useTransition } from "react";
 import List from "./components/List";
 import "./App.css";
 
 function App() {
   const [input, setInput] = useState<string>("");
+  const [isPending, startTransition] = useTransition();
+
+  function updateFilterHandler(value: string) {
+    startTransition(() => {
+      setInput(value);
+    });
+  }
 
   return (
     <div
       className="app"
       style={{ padding: 20, display: "flex", flexDirection: "column" }}
     >
-      <h4>useDeffered</h4>
+      <h4>useTransition</h4>
+
       <input
         style={{ width: 200 }}
-        onChange={(event) => setInput(event.target.value)}
+        onChange={(event) => updateFilterHandler(event.target.value)}
       />
+      {isPending && <p>Updating List...</p>}
       <List input={input} />
     </div>
   );
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useDeferredValue } from "react";
+import React, { useMemo } from "react";
 
 const LIST_SIZE = 2000;
 
@@ -7,16 +7,14 @@ type Props = {
 };
 
 const List: React.FC<Props> = ({ input }) => {
-  const deferredInput = useDeferredValue(input);
-
   const fullList = useMemo(() => {
     let list = [];
     for (let index = 0; index < LIST_SIZE; index++) {
-      list.push(<div key={`item-${index}`}>{deferredInput}</div>);
+      list.push(<div key={`item-${index}`}>{input}</div>);
     }
 
     return list;
-  }, [deferredInput]);
+  }, [input]);
 
   return <span>{fullList}</span>;
 };
